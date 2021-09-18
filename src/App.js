c77b33c85a134d2ed21be3593e1afae5c92fa6bb
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-09-18 19:09:14 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./logo.png"; 
 import './App.css';
+import Dictionary from "./Dictionary"; 
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
        
       </header>
+      <main>
+        <Dictionary />
+        </main>
+      <footer className="App-footer"> <small> Coded by Freya </small> </footer>
+      </div>
     </div>
   );
 }
