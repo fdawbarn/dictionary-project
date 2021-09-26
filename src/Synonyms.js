d63f6c69ee8b0fba5847b1886ab8719e65b51f57
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-09-26 09:44:47 +0100

    fixed bug

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,17 +1,15 @@
-import React from "react"; 
+import React from "react";
 
 export default function Synonyms(props) {
-if (props.synonyms) {
+  if (props.synonyms) {
     return (
-        <ul className="Synonyms">
-{props.synonyms.map(function (synonym, index) {
-    return 
-        <li key={index}>{synonym}</li>; 
- })}
-</ul>
-    ); 
-} else {
-    return null; 
-}
-
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
 }
