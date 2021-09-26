ffac618dec748d033ff7834a7099a4e39eb6aa63
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-09-26 19:42:20 +0100

    Updated css for each component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
        
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="plant" />
         </main>
       <footer className="App-footer"> <small> Coded by Freya </small> </footer>
       </div>
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,14 @@
 import React from "react"; 
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic"; 
+import "./Results.css"; 
 
 export default function Results(props) {
 if (props.results)  {
 
 return (
     <div className="Results"> 
+    <section>
     <h2>{props.results.word} </h2>
     {props.results.phonetics.map(function(phonetic, index) {
         return (
@@ -15,10 +17,11 @@ return (
     </div>
         );
     })}
+    </section>
     {props.results.meanings.map(function (meaning, index) {
-        return ( <div key={index}>
+        return ( <section key={index}>
             <Meaning meaning={meaning} />
-            </div>
+            </section>
         ); 
     })}
      </div>
